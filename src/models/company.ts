af83a0a885927d49d34af018f90715f452517dfc
Khalid <khalid@bawes.net>
2017-02-18 15:37:52 +0300

    Create company and staff models

---
+++ src/models/company.ts
@@ -0,0 +1,12 @@
+
+export interface Company{
+    company_id: number;
+    company_name: string;
+    company_email: string;
+    company_auth_key: string;
+    company_password_hash: string;
+    company_password_reset_token: string;
+    company_status: string;
+    company_created_at: string;
+    company_updated_at: string;
+}
---
+++ src/models/staff.ts
@@ -0,0 +1,12 @@
+
+export interface Staff{
+    staff_id: number;
+    staff_name: string;
+    staff_email: string;
+    staff_auth_key: string;
+    staff_password_hash: string;
+    staff_password_reset_token: string;
+    staff_status: string;
+    staff_created_at: string;
+    staff_updated_at: string;
+}
