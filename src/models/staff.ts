a02a900050d240871bc3689da8288cd81fb65d78
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-07-03 20:51:47 +0530

    small change

--- src/models/staff.ts
+++ src/models/staff.ts
@@ -4,7 +4,6 @@ export class Staff{
     staff_email: string;
     staff_password_hash: string;
     staff_status: string;
-    staff_password_hash: string;
     staff_created_at: string;
     staff_updated_at: string;
 }
