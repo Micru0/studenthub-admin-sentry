ac8c4b81a983c1faad457657036a43f60c437d32
Khalid <khalid@bawes.net>
2017-07-15 00:44:44 +0300

    Remove sliding candidate approval

--- src/pages/logged-in/candidate/candidate-review-list/candidate-review-list.ts
+++ src/pages/logged-in/candidate/candidate-review-list/candidate-review-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Events, NavController, LoadingController, ModalController, ToastController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 // Pages
 import { CandidateViewPage } from '../candidate-view/candidate-view';
@@ -25,33 +25,31 @@ export class CandidateReviewListPage {
   constructor(
     public navCtrl: NavController,
     public candidateService: CandidateService,
-    private _modalCtrl: ModalController,
-    private _loadingCtrl: LoadingController,
-    private _events: Events,
-    public toastCtrl: ToastController
+    private _loadingCtrl: LoadingController
   ) {}
 
-  ionViewWillEnter () {
+  ionViewWillEnter() {
     this.loadData(this.currentPage);
   }
 
+  /**
+   * Load Candidate List
+   * @param page 
+   */
   loadData(page: number){
-    // Load list of candidates
     let loader = this._loadingCtrl.create();
     loader.present();
-    this.candidateService.listToReview(page).subscribe(response => {
 
+    this.candidateService.listToReview(page).subscribe(response => {
       this.pageCount = response.headers.get('X-Pagination-Page-Count');
       this.currentPage = response.headers.get('X-Pagination-Current-Page');
 
       this.pages = [];
-
       for(var i = 1; i <= this.pageCount; i++){
          this.pages.push(i);
       }
 
       //hide if no page = 1 
-
       if(this.pageCount == 1)
         this.pages = [];
 
@@ -63,50 +61,19 @@ export class CandidateReviewListPage {
   }
 
   /**
-   * When its selected
+   * When a candidate is selected
    */
   rowSelected(model){
-    // Load Detail Page
     this.navCtrl.push(CandidateViewPage, {
       'model': model
     });
   }
 
   pageLinkColor(page: number) {
-
     if(page == this.currentPage) 
       return 'light';
     
     return '';
   }
 
-  /**
-   * Approve the provided model
-   */
-  approve(candidate: Candidate){
-    let loader = this._loadingCtrl.create();
-    loader.present();
-
-    this.candidateService.approve(candidate).subscribe(response => {
-      loader.dismiss();
-      
-      if (response.operation == 'error') {
-        var html = '';
-        for (let i in response.message) {
-          for (let j of response.message[i]) {
-             html += j;
-          }
-        }
-        let toast = this.toastCtrl.create({
-          message: html,
-          duration: 3000
-        });
-        toast.present();
-      } else {
-        //update review count 
-        this._events.publish('navigation:totalCandidateToReview');
-        this.loadData(this.currentPage);
-      }
-    });
-  }
 }
