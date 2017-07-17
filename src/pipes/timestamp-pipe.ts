9070afec4b3308f4f39c3e6028aee5912e079f83
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-07-17 12:33:28 +0530

    pipes diff. fodler

---
+++ src/pipes/timestamp-pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+    name: 'orderBy'
+})
+export class SortPipe implements PipeTransform {
+    transform(array: Array<any>, args?: any): any {
+        if (array != undefined) {
+            array.sort((a: any, b: any) => {
+
+                if(!a[args])
+                    return 0;
+                    
+                if (a[args].replace(/[-:]/g, '') < b[args].replace(/[-:]/g, '')) {
+                    return 1;
+                } else if (a[args].replace(/[-:]/g, '') > b[args].replace(/[-:]/g, '')) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+            return array;
+        }
+    }
+}
+
