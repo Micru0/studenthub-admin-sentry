4bccb3e39d731127ac3a65da55a873658b549e93
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-11-07 15:37:40 +0530

    admin ability to set company hourly rate + bonus commission

--- src/models/company.ts
+++ src/models/company.ts
@@ -4,6 +4,8 @@ export class Company {
     company_name: string;
     company_email: string;
     company_password_hash: string;
+    company_bonus_commission: number;
+    company_hourly_rate: number;
     company_status: string;
     total_candidates: number;
     subcompanies: Company[];
