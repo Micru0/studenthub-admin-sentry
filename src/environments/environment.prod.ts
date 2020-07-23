35d7a5a6da60a502797fb1312e5ae87ccd16a303
Krushn <kathrechakrushn@gmail.com>
2020-07-23 18:38:59 +0530

    endpoint fr production updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   serviceWorker: true,
   envName: 'prod',
-  apiEndpoint: 'https://payroll-admin.studenthub.co/v1',
+  apiEndpoint: 'https://admin.api.studenthub.co/v1',
   permanentBucketUrl: "https://studenthub-uploads.s3.amazonaws.com/",
 };
