e46a2fe65954149de9a7b6ed1b7c418174224045
Krushn <kathrechakrushn@gmail.com>
2020-08-06 16:22:53 +0530

    ability to view transfer confirmation details

--- src/app/models/transfer-candidate.ts
+++ src/app/models/transfer-candidate.ts
@@ -1,13 +1,19 @@
 import { Candidate } from './candidate';
+import { Bank } from './bank';
+import { Store } from './store';
+import { Company } from './company';
 
 export class TransferCandidate {
     tc_id: number;
-    transfer_confirmation_id: any;
     transfer_id: number; 
     candidate_id: number;
     store_id: number;
     store_name: string;
     company_id: number;
+    bank_id: number;
+    transfer_confirmation_id: any;
+    transfer_benef_name: any;
+    transfer_benef_iban: any;
     company_name: string;
     company_email: string;
     candidate_hourly_rate: number;
@@ -23,4 +29,7 @@ export class TransferCandidate {
     total_amount: number;
     profit: number;
     candidate: Candidate;
+    bank: Bank;
+    store: Store;
+    company: Company;
 }
