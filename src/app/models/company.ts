d31191dbdca118f61f41488b711f5315b3d783a8
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-17 17:26:31 +0530

    [Finishes #174323435] active inactive segment is created

--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -6,8 +6,10 @@ export class Company {
     company_password_hash: string;
     company_bonus_commission: number;
     company_hourly_rate: number;
-    company_status: string;
+    company_status: number;
     total_candidates: number;
+    total_stores: number;
+    total_subcompanies: number;
     subCompanies: Company[];
     files: any[];
 }
