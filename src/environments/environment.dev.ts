754c4ba877d2e897fcb1b098e77f1dc2b86cca48
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-09-02 12:20:37 +0530

    [Finishes #174596278] change dev and local cloudinary path

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   serviceWorker: true,
   envName: 'dev',
-  cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/',
+  cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/dev/',
   permanentBucketUrl: "https://studenthub-uploads-dev-server.s3.amazonaws.com/",
   apiEndpoint: 'https://admin.api.dev.studenthub.co/v1'
 };
