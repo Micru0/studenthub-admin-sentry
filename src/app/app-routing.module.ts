3b87c165fb466634c0e4022aa01094c1a1a5df66
Krushn Kathrecha <kathrechakrushn@gmail.com>
2020-09-23 19:31:12 +0530

    selective loading for modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { SelectiveLoadingStrategy } from './util/SelectiveLoadingStrategy';
 //services
 import { AuthService } from './providers/auth.service';
-import {LoginGuard} from "./providers/guards/login-guard.service";
+import { LoginGuard } from "./providers/guards/login-guard.service";
 
 
 const routes: Routes = [
@@ -34,195 +35,304 @@ const routes: Routes = [
   },
   {
     path: 'bank-list',
-    loadChildren: () => import('./pages/logged-in/bank/bank-list/bank-list.module').then( m => m.BankListPageModule),
+    loadChildren: () => import('./pages/logged-in/bank/bank-list/bank-list.module').then(m => m.BankListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'BankListPage',
+    }
   },
   {
     path: 'bank-form',
-    loadChildren: () => import('./pages/logged-in/bank/bank-form/bank-form.module').then( m => m.BankFormPageModule),
+    loadChildren: () => import('./pages/logged-in/bank/bank-form/bank-form.module').then(m => m.BankFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'BankFormPage',
+    }
   },
   {
     path: 'bank-view',
-    loadChildren: () => import('./pages/logged-in/bank/bank-view/bank-view.module').then( m => m.BankViewPageModule),
+    loadChildren: () => import('./pages/logged-in/bank/bank-view/bank-view.module').then(m => m.BankViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'BankViewPage',
+    }
   },
   {
     path: 'candidate-view',
-    loadChildren: () => import('./pages/logged-in/candidate/candidate-view/candidate-view.module').then( m => m.CandidateViewPageModule),
+    loadChildren: () => import('./pages/logged-in/candidate/candidate-view/candidate-view.module').then(m => m.CandidateViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CandidateViewPage',
+    }
   },
   {
     path: 'candidate-review-list',
-    loadChildren: () => import('./pages/logged-in/candidate/candidate-review-list/candidate-review-list.module').then( m => m.CandidateReviewListPageModule),
+    loadChildren: () => import('./pages/logged-in/candidate/candidate-review-list/candidate-review-list.module').then(m => m.CandidateReviewListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CandidateReviewPage',
+    }
   },
   {
     path: 'company-form',
-    loadChildren: () => import('./pages/logged-in/company/company-form/company-form.module').then( m => m.CompanyFormPageModule),
+    loadChildren: () => import('./pages/logged-in/company/company-form/company-form.module').then(m => m.CompanyFormPageModule),
     canActivate: [AuthService],
   },
   {
     path: 'company-list',
-    loadChildren: () => import('./pages/logged-in/company/company-list/company-list.module').then( m => m.CompanyListPageModule),
+    loadChildren: () => import('./pages/logged-in/company/company-list/company-list.module').then(m => m.CompanyListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CompanyListPage',
+    }
   },
   {
     path: 'company-view',
-    loadChildren: () => import('./pages/logged-in/company/company-view/company-view.module').then( m => m.CompanyViewPageModule),
+    loadChildren: () => import('./pages/logged-in/company/company-view/company-view.module').then(m => m.CompanyViewPageModule),
     canActivate: [AuthService],
     data: {
+      name: 'CompanyViewPage',
       navDisable: true,
     }
   },
   {
     path: 'country-list',
-    loadChildren: () => import('./pages/logged-in/country/country-list/country-list.module').then( m => m.CountryListPageModule),
+    loadChildren: () => import('./pages/logged-in/country/country-list/country-list.module').then(m => m.CountryListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CompanyListPage'
+    }
   },
   {
     path: 'country-view',
-    loadChildren: () => import('./pages/logged-in/country/country-view/country-view.module').then( m => m.CountryViewPageModule),
+    loadChildren: () => import('./pages/logged-in/country/country-view/country-view.module').then(m => m.CountryViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CountryViewPage'
+    }
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./pages/logged-in/dashboard/dashboard.module').then( m => m.DashboardPageModule),
+    loadChildren: () => import('./pages/logged-in/dashboard/dashboard.module').then(m => m.DashboardPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'DashboardPage'
+    }
   },
   {
     path: 'staff-list',
-    loadChildren: () => import('./pages/logged-in/staff/staff-list/staff-list.module').then( m => m.StaffListPageModule),
+    loadChildren: () => import('./pages/logged-in/staff/staff-list/staff-list.module').then(m => m.StaffListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'StaffListPage'
+    }
   },
   {
     path: 'staff-view',
-    loadChildren: () => import('./pages/logged-in/staff/staff-view/staff-view.module').then( m => m.StaffViewPageModule),
+    loadChildren: () => import('./pages/logged-in/staff/staff-view/staff-view.module').then(m => m.StaffViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'StaffViewPage'
+    }
   },
   {
     path: 'staff-form',
-    loadChildren: () => import('./pages/logged-in/staff/staff-form/staff-form.module').then( m => m.StaffFormPageModule),
+    loadChildren: () => import('./pages/logged-in/staff/staff-form/staff-form.module').then(m => m.StaffFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'StaffFormPage'
+    }
   },
   {
     path: 'admin-list',
-    loadChildren: () => import('./pages/logged-in/admin/admin-list/admin-list.module').then( m => m.AdminListPageModule),
+    loadChildren: () => import('./pages/logged-in/admin/admin-list/admin-list.module').then(m => m.AdminListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'AdminListPage'
+    }
   },
   {
     path: 'admin-view',
-    loadChildren: () => import('./pages/logged-in/admin/admin-view/admin-view.module').then( m => m.AdminViewPageModule),
+    loadChildren: () => import('./pages/logged-in/admin/admin-view/admin-view.module').then(m => m.AdminViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'AdminViewPage'
+    }
   },
   {
     path: 'admin-form',
-    loadChildren: () => import('./pages/logged-in/admin/admin-form/admin-form.module').then( m => m.AdminFormPageModule),
+    loadChildren: () => import('./pages/logged-in/admin/admin-form/admin-form.module').then(m => m.AdminFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'AdminFormPage'
+    }
   },
   {
     path: 'store-view',
-    loadChildren: () => import('./pages/logged-in/store/store-view/store-view.module').then( m => m.StoreViewPageModule),
+    loadChildren: () => import('./pages/logged-in/store/store-view/store-view.module').then(m => m.StoreViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'StoreViewPage'
+    }
   },
   {
     path: 'candidate-payment-search',
-    loadChildren: () => import('./pages/logged-in/transfer/candidate-payment-search/candidate-payment-search.module').then( m => m.CandidatePaymentSearchPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/candidate-payment-search/candidate-payment-search.module').then(m => m.CandidatePaymentSearchPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CandidatePaymentSearchPage'
+    }
   },
   {
     path: 'candidate-transfer-detail',
-    loadChildren: () => import('./pages/logged-in/transfer/candidate-transfer-detail/candidate-transfer-detail.module').then( m => m.CandidateTransferDetailPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/candidate-transfer-detail/candidate-transfer-detail.module').then(m => m.CandidateTransferDetailPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CandidateTransferDetailPage'
+    }
   },
   {
     path: 'candidate-transfer-list',
-    loadChildren: () => import('./pages/logged-in/transfer/candidate-transfer-list/candidate-transfer-list.module').then( m => m.CandidateTransferListPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/candidate-transfer-list/candidate-transfer-list.module').then(m => m.CandidateTransferListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'CandidateTransferListPage'
+    }
   },
   {
     path: 'payable-candidates',
-    loadChildren: () => import('./pages/logged-in/transfer/payable-candidates/payable-candidates.module').then( m => m.PayableCandidatesPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/payable-candidates/payable-candidates.module').then(m => m.PayableCandidatesPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'PayableCandidatesPage'
+    }
   },
 
   {
     path: 'import-transfer-form',
     loadChildren: () => import('./pages/logged-in/transfer/import-transfer-form/import-transfer-form.module').then(m => m.ImportTransferFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'ImportTransferFormPage'
+    }
   },
   {
     path: 'transfer-list',
-    loadChildren: () => import('./pages/logged-in/transfer/transfer-list/transfer-list.module').then( m => m.TransferListPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/transfer-list/transfer-list.module').then(m => m.TransferListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'TransferListPage'
+    }
   },
   {
     path: 'transfer-paid',
-    loadChildren: () => import('./pages/logged-in/transfer/transfer-paid/transfer-paid.module').then( m => m.TransferPaidPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/transfer-paid/transfer-paid.module').then(m => m.TransferPaidPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'TransferPaidPage'
+    }
   },
   {
     path: 'transfer-view',
-    loadChildren: () => import('./pages/logged-in/transfer/transfer-view/transfer-view.module').then( m => m.TransferViewPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/transfer-view/transfer-view.module').then(m => m.TransferViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'TransferViewPage'
+    }
   },
   {
     path: 'university-list',
-    loadChildren: () => import('./pages/logged-in/university/university-list/university-list.module').then( m => m.UniversityListPageModule),
+    loadChildren: () => import('./pages/logged-in/university/university-list/university-list.module').then(m => m.UniversityListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'UniversityListPage'
+    }
   },
   {
     path: 'university-view',
-    loadChildren: () => import('./pages/logged-in/university/university-view/university-view.module').then( m => m.UniversityViewPageModule),
+    loadChildren: () => import('./pages/logged-in/university/university-view/university-view.module').then(m => m.UniversityViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'UniversityViewPage'
+    }
   },
   {
     path: 'university-form',
-    loadChildren: () => import('./pages/logged-in/university/university-form/university-form.module').then( m => m.UniversityFormPageModule),
+    loadChildren: () => import('./pages/logged-in/university/university-form/university-form.module').then(m => m.UniversityFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'UniversityFormPage'
+    }
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/start-pages/login/login.module').then( m => m.LoginPageModule),
-    canActivate: [LoginGuard]
+    loadChildren: () => import('./pages/start-pages/login/login.module').then(m => m.LoginPageModule),
+    canActivate: [LoginGuard],
+    data: {
+      name: 'LoginPage'
+    }
   },
   {
     path: 'transfer-file-list',
-    loadChildren: () => import('./pages/logged-in/transfer/transfer-file-list/transfer-file-list.module').then( m => m.TransferFileListPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/transfer-file-list/transfer-file-list.module').then(m => m.TransferFileListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'TransferFileListPage'
+    }
   },
   {
     path: 'transfer-file-detail',
-    loadChildren: () => import('./pages/logged-in/transfer/transfer-file-detail/transfer-file-detail.module').then( m => m.TransferFileDetailPageModule),
+    loadChildren: () => import('./pages/logged-in/transfer/transfer-file-detail/transfer-file-detail.module').then(m => m.TransferFileDetailPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'TransferFileDetailPage'
+    }
   },
   {
     path: 'brand-form',
-    loadChildren: () => import('./pages/logged-in/company/brand-form/brand-form.module').then( m => m.BrandFormPageModule)
+    loadChildren: () => import('./pages/logged-in/company/brand-form/brand-form.module').then(m => m.BrandFormPageModule),
+    data: {
+      name: 'BrandFormPage'
+    }
   },
   {
     path: 'company-contact-form',
-    loadChildren: () => import('./pages/logged-in/company/company-contact-form/company-contact-form.module').then( m => m.CompanyContactFormPageModule)
+    loadChildren: () => import('./pages/logged-in/company/company-contact-form/company-contact-form.module').then(m => m.CompanyContactFormPageModule),
+    data: {
+      name: 'CompanyContactFormPage'
+    }
   },
   {
     path: 'inspector-list',
-    loadChildren: () => import('./pages/logged-in/inspector/inspector-list/inspector-list.module').then( m => m.InspectorListPageModule),
+    loadChildren: () => import('./pages/logged-in/inspector/inspector-list/inspector-list.module').then(m => m.InspectorListPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'InspectorListPage'
+    }
   },
   {
     path: 'inspector-view',
-    loadChildren: () => import('./pages/logged-in/inspector/inspector-view/inspector-view.module').then( m => m.InspectorViewPageModule),
+    loadChildren: () => import('./pages/logged-in/inspector/inspector-view/inspector-view.module').then(m => m.InspectorViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'InspectorViewPage'
+    }
   },
   {
     path: 'inspector-form',
-    loadChildren: () => import('./pages/logged-in/inspector/inspector-form/inspector-form.module').then( m => m.InspectorFormPageModule),
+    loadChildren: () => import('./pages/logged-in/inspector/inspector-form/inspector-form.module').then(m => m.InspectorFormPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'InspectorFormPage'
+    }
   },
   {
     path: 'brand-view',
-    loadChildren: () => import('./pages/logged-in/company/brand-view/brand-view.module').then( m => m.BrandViewPageModule),
+    loadChildren: () => import('./pages/logged-in/company/brand-view/brand-view.module').then(m => m.BrandViewPageModule),
     canActivate: [AuthService],
+    data: {
+      name: 'BrandViewPage'
+    }
   },
   {
     path: '**',
@@ -233,8 +343,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { enableTracing: false, preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { enableTracing: false, preloadingStrategy: SelectiveLoadingStrategy })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule, APP_INITIALIZER, ErrorHandler} from '@angular/core';
+import { NgModule, APP_INITIALIZER, Injector, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -29,7 +29,7 @@ import { SentryErrorhandlerService } from './providers/sentry.errorhandler.servi
 import { UploadFilePageModule } from './pages/logged-in/company/upload-file/upload-file.module';
 import { BrandFormPageModule } from './pages/logged-in/company/brand-form/brand-form.module';
 import { CompanyContactFormPageModule } from './pages/logged-in/company/company-contact-form/company-contact-form.module';
-import {NoItemsModule} from "./components/no-items/no-items.module";
+import { NoItemsModule } from "./components/no-items/no-items.module";
 
 export function startupServiceFactory(authService) {
   return () => authService.load();
@@ -78,4 +78,11 @@ export function startupServiceFactory(authService) {
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { 
+
+  static injector: Injector;
+
+  constructor(public injector: Injector) {
+    AppModule.injector = injector;
+  }
+}
