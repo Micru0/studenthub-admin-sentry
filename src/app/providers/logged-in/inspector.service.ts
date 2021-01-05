8c0a8b01be6260855dfe9a8dc93f420226ad66b9
Anil Kumar <anilkumar.dhiman1@gmail.com>
2021-01-05 17:50:21 +0530

    [Finishes #176284040] max width set for staff app and team note view list issue fixed

--- src/app/providers/logged-in/inspector.service.ts
+++ src/app/providers/logged-in/inspector.service.ts
@@ -42,7 +42,7 @@ export class InspectorService {
     return this.authHttp.post(`${this.endpoint}`, {
       name : model.inspector_name,
       email : model.inspector_email,
-      inspector_password_hash : model.inspector_password_hash,
+      password : model.inspector_password_hash,
     });
   }
 
