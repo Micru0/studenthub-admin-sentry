7dc141eb619a4cc08ee7f595edfdad49e0753d03
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-06-22 14:22:31 +0530

    staff view changes done

--- src/app/models/staff.ts
+++ src/app/models/staff.ts
@@ -6,6 +6,7 @@ export class Staff{
     staff_gmail_username: string;
     staff_gmail_password: string;
     staff_status: string;
+    staff_notification: string;
     staff_created_at: string;
     staff_updated_at: string;
     deleted: number;
