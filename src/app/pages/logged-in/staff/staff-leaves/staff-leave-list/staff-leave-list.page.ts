ea926c798e0e17b04bbd0eb22fb9d8bbfb991ab7
Anil Kumar <anilkumar.dhiman1@gmail.com>
2023-02-07 12:04:41 +0530

    leave management is working now

---
+++ src/app/pages/logged-in/staff/staff-leaves/staff-leave-list/staff-leave-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-leave-list',
+  templateUrl: './staff-leave-list.page.html',
+  styleUrls: ['./staff-leave-list.page.scss'],
+})
+export class StaffLeaveListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/logged-in/staff/staff-work-session/staff-work-session-list/staff-work-session-list.module.ts
+++ src/app/pages/logged-in/staff/staff-work-session/staff-work-session-list/staff-work-session-list.module.ts
@@ -7,9 +7,9 @@ import { IonicModule } from '@ionic/angular';
 import { StaffWorkSessionListPageRoutingModule } from './staff-work-session-list-routing.module';
 
 import { StaffWorkSessionListPage } from './staff-work-session-list.page';
-import {LoadingModalModule} from "../../../../components/loading-modal/loading-modal.module";
-import {NoItemsModule} from "../../../../components/no-items/no-items.module";
-import {PipesModule} from "../../../../pipes/pipes.module";
+import {LoadingModalModule} from "src/app/components/loading-modal/loading-modal.module";
+import {NoItemsModule} from "src/app/components/no-items/no-items.module";
+import {PipesModule} from "src/app/pipes/pipes.module";
 
 @NgModule({
   imports: [
